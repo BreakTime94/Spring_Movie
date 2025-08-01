e068d43e7ef9afc1fc13e2657fdaabc1620e115f
Kimchan <kimchan9408@gmail.com>
2025-08-01 18:19:45 +0900

    8교시 실습시간, swagger controller 시도

---
+++ src/main/java/com/climbjava/mreview/controller/SwaggerController.java
@@ -0,0 +1,14 @@
+package com.climbjava.mreview.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("swag")
+public class SwaggerController {
+  @GetMapping("review")
+  public String openReviewSwag() {
+    return "/swagger-ui/index.html#/review-controller";
+  }
+}
